6121c2f10abc4a130e51f19cb9858ddea295c6f7
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-12-02 21:47:07 +0800

    receives a name, returns 'Hello <name>' trimmed of white space

--- greeter.js
+++ greeter.js
@@ -1,11 +1,5 @@
-const greeter = (name, time) => {
-    const hourOfDay = time.getHours();
-    const nameTrimmed = name.trim();
-    const nameCapitalised = nameTrimmed.charAt(0).toUpperCase() + nameTrimmed.slice(1);
-    if (hourOfDay >= 6 && hourOfDay < 12) return `Good Morning ${nameCapitalised}`;
-    if (hourOfDay >= 12 && hourOfDay < 18) return `Hello ${nameCapitalised}`
-    if (hourOfDay >= 18 && hourOfDay < 22) return `Good Evening ${nameCapitalised}`;
-    return `Good Night ${nameCapitalised}`;
-};
+const greeter = name => {
+    return `Hello ${name.trim()}`
+}
 
 export default greeter;
--- greeter.test.js
+++ greeter.test.js
@@ -1,29 +1,10 @@
-import greeter from "./greeter";
-
-const date = new Date();
-date.setHours(15);
-date.setMinutes(0);
+import greeter from "./greeter"
 
 describe("greeter", () => {
-    it("receives name and returns 'hello name'", () => {
-        expect(greeter("Robyn", date)).toBe("Hello Robyn");
-    });
-    it("returns trimmed input", () => {
-        expect(greeter("    Robyn    ", date)).toBe("Hello Robyn")
-    });
-    it("returns first letter of name capitalised", () => {
-        expect(greeter("robyn", date)).toBe("Hello Robyn");
-    });
-    it("returns Good Morning <name> if time is 06:00 - 12:00", () => {
-        date.setHours(7);
-        expect(greeter("Robyn", date)).toBe("Good Morning Robyn");
-    });
-    it("returns Good Evening <name> if time is 18:00 - 22:00", () => {
-        date.setHours(18);
-        expect(greeter("Robyn", date)).toBe("Good Evening Robyn");
-    });
-    it("returns Good Night <name> if time is 22:00 - 6:00", () => {
-        date.setHours(2);
-        expect(greeter("Robyn", date)).toBe("Good Night Robyn");
-    });
+    it("receives a name, returns 'Hello <name>", () => {
+        expect(greeter("Robyn")).toBe("Hello Robyn")
+    })
+    it("receives a name, returns 'Hello <name>' trimmed of white space", () => {
+        expect(greeter("       Robyn            ")).toBe("Hello Robyn")
+    })
 })
