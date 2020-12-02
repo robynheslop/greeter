e0136a8a2865c225b0eae44d84b7333beb7f32b0
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-12-02 22:13:07 +0800

    returns 'Good Evening <name> if the time is 18:00:00

--- greeter.js
+++ greeter.js
@@ -8,8 +8,10 @@ const formatName = name => {
 const greeter = (name, time) => {
     if (time) {
         let hour = time.getHours();
-        if (hour >= 6) {
+        if (hour >= 6 && hour < 12) {
             return `Good Morning ${formatName(name)}`
+        } else if (hour >= 18 && hour < 22) {
+            return `Good Evening ${formatName(name)}`
         }
     }
     return `Hello ${formatName(name)}`
--- greeter.test.js
+++ greeter.test.js
@@ -3,18 +3,26 @@ import greeter from "./greeter"
 describe("greeter", () => {
     it("receives a name, returns 'Hello <name>", () => {
         expect(greeter("Robyn")).toBe("Hello Robyn")
-    })
+    });
     it("receives a name, returns 'Hello <name>' trimmed of white space", () => {
         expect(greeter("       Robyn            ")).toBe("Hello Robyn")
-    })
+    });
     it("receives a name, returns 'Hello <name>' with first letter of name capitalied", () => {
         expect(greeter("robyn")).toBe("Hello Robyn")
-    })
+    });
     it("receives a full name, returns 'Hello <full name>' with first letter of each name capitalied", () => {
         expect(greeter("robyn heslop")).toBe("Hello Robyn Heslop")
-    })
-    it("returns 'Good Morning <name> if the time is 06:00", () => {
+    });
+    it("returns 'Good Morning <name> if the time is 06:00:00", () => {
         let time = new Date('December 17, 1995 06:00:00')
         expect(greeter("Robyn", time)).toBe("Good Morning Robyn")
-    })
+    });
+    it("returns 'Good Morning <name> if the time is 11:59:00", () => {
+        let time = new Date('December 17, 1995 11:59:00')
+        expect(greeter("Robyn", time)).toBe("Good Morning Robyn")
+    });
+    it("returns 'Good Evening <name> if the time is 18:00:00", () => {
+        let time = new Date('December 17, 1995 18:00:00')
+        expect(greeter("Robyn", time)).toBe("Good Evening Robyn")
+    });
 })
