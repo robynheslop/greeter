0d7287437b087858376e0a2fe462a7f09d68bf04
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-12-02 21:58:15 +0800

    receives a full name, returns 'Hello <full name>' with first letter of each name capitalied

--- greeter.js
+++ greeter.js
@@ -1,5 +1,12 @@
+const formatName = name => {
+    let trimmedName = name.trim();
+    let nameArray = trimmedName.split(" ");
+    let capitalisedName = nameArray.map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(" ");
+    return capitalisedName;
+}
+
 const greeter = name => {
-    return `Hello ${name.trim()}`
+    return `Hello ${formatName(name)}`
 }
 
 export default greeter;
--- greeter.test.js
+++ greeter.test.js
@@ -7,4 +7,10 @@ describe("greeter", () => {
     it("receives a name, returns 'Hello <name>' trimmed of white space", () => {
         expect(greeter("       Robyn            ")).toBe("Hello Robyn")
     })
+    it("receives a name, returns 'Hello <name>' with first letter of name capitalied", () => {
+        expect(greeter("robyn")).toBe("Hello Robyn")
+    })
+    it("receives a full name, returns 'Hello <full name>' with first letter of each name capitalied", () => {
+        expect(greeter("robyn heslop")).toBe("Hello Robyn Heslop")
+    })
 })
